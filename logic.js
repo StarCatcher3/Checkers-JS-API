922727936c5eb18ba14f85f83acf8432bb27a5cb
John-Rep <replogle.john@next-u.fr>
2025-06-29 17:40:25 +0200

    Room assignment

--- logic.js
+++ logic.js
@@ -1,10 +1,48 @@
-let rooms = [];
+let rooms = new Map();
+let roomSizes = new Map();
 let roomCount = 0;
 
-function joinRoom() {
-    rooms.push({id: roomCount, players: [1]});
+const { clients } = require('./state');
+
+function JoinRoom(userId, roomSize) {
+    console.log(userId + " is joining a room of size " + roomSize);
+    socket = clients.get(userId);
+    if (!socket) {
+        return "Client not connected";
+    }
+    for (const [key, value] of rooms) {
+        if (value.has(userId)) {
+            socket.send(JSON.stringify({
+                message: "User already in room " + key
+            }));
+            return;
+        }
+    }
+    console.log(userId + " is not already in a room");
+    for (const [key, value] of rooms) {
+        if (roomSizes.get(key) == roomSize && value.size < roomSize) {
+            socket.send(JSON.stringify({
+                message: "User joining room " + key
+            }));
+            value.add(userId);
+            return;
+        }
+    }
+
+
+    console.log("A new room is being created for " + userId);
+    roomUsers = new Set();
+    roomUsers.add(userId);
+    rooms.set(roomCount, roomUsers);
+    roomSizes.set(roomCount, roomSize);
+    socket.send(JSON.stringify({
+        message: "User joining room " + roomCount
+    }));
     roomCount++;
-    return rooms.at(-1).id;
+    rooms.forEach((value, key) => {
+        console.log("Room " + key + ": size" + roomSizes.get(key) + ": " + Array.from(value).toString());
+    });
+    return;
 }
 
-module.exports = { joinRoom }
+module.exports = { JoinRoom }
--- socket.js
+++ socket.js
@@ -1,50 +1,70 @@
-let clients = new Map();
+
 let clientCount = 0;
+const sessionId = Math.random().toString(36).substring(2,6);
 
-function StartSocket() {
-    const WebSocket = require('ws');
-    const server = new WebSocket.Server({ port: 3001 });
+const { JoinRoom } = require('./logic');
+const { clients } = require('./state');
 
-    server.on('connection', (socket) => {
-        socket.userId = "user" + clientCount;
-        clientCount++;
-        console.log("Client connected: " + socket.userId);
-        socket.send(JSON.stringify(
-            {
-                userId: socket.userId
-            }
-        ))
-        clients.set(socket.userId, socket);
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 3000 });
 
-        socket.on('message', (message) => {
-            try {
-                data = JSON.parse(message);
-                if (data.oldUserId && data.newUserId) {
+server.on('connection', (socket) => {
+    socket.userId = sessionId + clientCount;
+    clientCount++;
+    console.log("Client connected: " + socket.userId);
+    socket.send(JSON.stringify(
+        {
+            userId: socket.userId
+        }
+    ))
+    clients.set(socket.userId, socket);
+
+    socket.on('message', (message) => {
+        try {
+            data = JSON.parse(message);
+
+            // Assign UserId from an existing session
+            if (data.oldUserId && data.newUserId) {
+                if (data.oldUserId.substring(0, 4) == sessionId) {
                     socket.userId = data.oldUserId;
                     clients.delete(data.newUserId);
                     clients.set(data.oldUserId, socket);
+                } else {
+                    socket.send(JSON.stringify(
+                        {
+                            userId: socket.userId,
+                            forceUserId: true
+                        }
+                    ))
                 }
-                if (data.message) {
-                    console.log("Client Message: " + data.message);
-                    socket.send(JSON.stringify({
-                        message: "Message Received from " + socket.userId + "!"
-                    }));
-                }
+            }
+
+            if (data.joinRoom && data.userId && data.roomSize) {
+                JoinRoom(data.userId, data.roomSize);
+            }
+
+            // Show the message received in the console
+            if (data.message) {
+                console.log("Client Message: " + data.message);
                 socket.send(JSON.stringify({
-                    message: "All Connected Clients: " + Array.from(clients.keys()).toString()
-                }));
-            } catch (error) {
-                socket.send(JSON.stringify({
-                    message: error.message
+                    message: "Message Received from user " + socket.userId + "!"
                 }));
             }
-        })
+            
+            /* 
+            socket.send(JSON.stringify({
+                message: "All Connected Clients: " + Array.from(clients.keys()).toString()
+            })); */
 
-        socket.on('close', () => {
-            console.log("Client disconnected: " + socket.userId);
-            clients.delete(socket.userId); // to be moved elsewhere
-        })
+        } catch (error) {
+            socket.send(JSON.stringify({
+                message: error.message
+            }));
+        }
     })
-}
 
-module.exports = { StartSocket, clients }
+    socket.on('close', () => {
+        console.log("Client disconnected: " + socket.userId);
+        clients.delete(socket.userId); // to be moved elsewhere
+    })
+})
